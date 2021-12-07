3ca0f7b140c0a8f8e3cd025b1f8c677a89f1274a
Tony Krikorian <tony.krikorian@outlook.com>
2021-12-07 19:27:09 -0300

    Express webserver

---
+++ buildScripts/srcServer.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+const open = require("open");
+
+const port = 3000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../src/index.html"));
+});
+
+app.listen(port, (err) => {
+  if (err) console.log(err);
+  if (!err) open(`http://localhost:${port}`);
+});
