598c47bd9a2617a20a76f34783de6dd64e9d0681
Tony Krikorian <tony.krikorian@outlook.com>
2021-12-09 21:23:44 -0300

    add webpack

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,10 +1,19 @@
-const express = require("express");
-const path = require("path");
-const open = require("open");
+import express from "express";
+import path from "path";
+import open from "open";
+import webpack from "webpack";
+import config from "../webpack.config.dev";
 
 const port = 3000;
-
 const app = express();
+const compiler = webpack(config);
+
+app.use(
+  require("webpack-dev-middleware")(compiler, {
+    publicPath: config.output.publicPath,
+    noInfo: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../src/index.html"));
