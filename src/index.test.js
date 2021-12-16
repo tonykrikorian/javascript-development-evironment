defc26ab2e2baa0deca558e8ad10415002e2fe00
Tony Krikorian <tony.krikorian@outlook.com>
2021-12-15 21:01:24 -0300

    add automatic lint

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,20 @@
-const { expect } = require("chai");
+import { expect } from "chai";
+import jsdom from "jsdom";
+import fs from "fs";
 
 describe("Our first test", () => {
   it("Shoud pass", () => {
     expect(true).to.equal(true);
+  });
+});
+
+describe("index.html", () => {
+  it("Should say hello", () => {
+    const index = fs.readFileSync("./src/index.html", "utf-8");
+    const { JSDOM } = jsdom;
+    const dom = new JSDOM(index);
+    const [h1] = dom.window.document.getElementsByTagName("h1");
+    expect(h1.innerHTML).to.equal("Hello World");
+    dom.window.close();
   });
 });
